05a89cc90e5609616f97656e0f81b0cafecef36a
mostafamohamedsalah25 <mostafamohamedsalah25@gmail.com>
2026-04-19 14:55:12 +0200

    feat: register Course and Trainee models in Django admin

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from course.models import Course
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'description', 'capacity')
+    search_fields = ('name',)
--- trainee/admin.py
+++ trainee/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from trainee.models import Trainee
 # Register your models here.
+@admin.register(Trainee)
+class TraineeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'track', 'branch', 'course')
+    list_filter = ('track', 'branch', 'course')
+    search_fields = ('first_name', 'last_name', 'email')
