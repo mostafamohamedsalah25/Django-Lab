953671f0e75f8436b2724e102f8587bcb788ad69
mostafamohamedsalah25 <mostafamohamedsalah25@gmail.com>
2026-04-19 15:22:53 +0200

    feat: implement trainee list view, urls, and templates

---
+++ trainee/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.trainee_list, name='trainee_list'),
+]
--- trainee/views.py
+++ trainee/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Trainee
 # Create your views here.
+def trainee_list(request):
+    trainees = Trainee.objects.all()
+    context = {'trainees': trainees}
+    return render(request, 'list.html', context)
